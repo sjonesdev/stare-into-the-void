6b2a105981d9f79fa2bc2fc29a6db99a962caee1
SamJones329 <55857337+SamJones329@users.noreply.github.com>
2022-09-18 22:23:54 -0500

    fixed issue with previous fix

--- src/index.tsx
+++ src/index.tsx
@@ -23,9 +23,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-// const app = 
-initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig);
+/* const analytics =*/ getAnalytics(app);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
