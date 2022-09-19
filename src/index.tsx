4b5105767de757ccfeccb57aaf522297ef37a33c
SamJones329 <55857337+SamJones329@users.noreply.github.com>
2022-09-18 22:13:44 -0500

    fixed unused stuff

--- src/index.tsx
+++ src/index.tsx
@@ -23,8 +23,9 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const app = 
+initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
