b22ce0d8d8c8482a94d509029647f4f44ebc32e7
SamJones329 <55857337+SamJones329@users.noreply.github.com>
2022-09-22 15:23:25 -0500

    Removed CORS restrictions by using cors middleware package.

--- src/index.ts
+++ src/index.ts
@@ -12,15 +12,22 @@ admin.initializeApp();
 //   response.send("Hello from Firebase!");
 // });
 // const functions = require('firebase-functions');
+// const cors = require("cors")({origin: true});
+import * as cors from "cors";
+// TODO - configure CORS to have proper domain control
+// instead of allowing all origins
+const handleCors = cors({origin: true});
 
-exports.bigben = functions.https.onRequest((_, res) => {
-  const hours = (new Date().getHours() % 12) + 1; // London is UTC + 1hr;
-  res.status(200).send(`<!doctype html>
-    <head>
-      <title>Time</title>
-    </head>
-    <body>
-      ${"BONG ".repeat(hours)}
-    </body>
-  </html>`);
+exports.bigben = functions.https.onRequest((req, res) => {
+  handleCors(req, res, () => {
+    const hours = (new Date().getHours() % 12) + 1; // London is UTC + 1hr;
+    res.status(200).send(`<!doctype html>
+        <head>
+          <title>Time</title>
+        </head>
+        <body>
+          ${"BONG ".repeat(hours)}
+        </body>
+      </html>`);
+  });
 });
