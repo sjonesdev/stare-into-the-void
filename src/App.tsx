da4f1e858f1b04ec7ac3bc4b10db307b8d3ffc37
SamJones329 <55857337+SamJones329@users.noreply.github.com>
2022-10-27 12:44:43 -0500

    added react router and some functional components for the top of each route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+} from "react-router-dom";
 import Navbar from './components/Navbar';
 import './App.css';
 
+import Home from './routes/home/Home';
+import About from './routes/about/About';
+import Edit from './routes/edit/Edit';
+import Recent from './routes/recent/Recent';
+
+
 function App() {
+
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/">
+      <Route index element={<> <Navbar /> <Home /> </>} />
+      <Route path="about" element={<> <Navbar /> <About /> </>} />
+      <Route path="edit" element={<> <Navbar /> <Edit /> </>}/>
+      <Route path="recent" element={<> <Navbar /><Recent /> </>}/>
+    </Route>
+  ))
   return (
     <div className="App">
-      <header className="App-header">
-        <Navbar />
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/routes/about/About.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+
+export default function About() {
+  return <div>Hello World!</div>
+}
