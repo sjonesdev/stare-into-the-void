55c1fa2964d9e62032f45769d32f46c8951e6f89
SamJones329 <55857337+SamJones329@users.noreply.github.com>
2022-11-01 10:38:20 -0500

    added combobox for selecting a source api

--- src/routes/browse/Browse.tsx
+++ src/routes/browse/Browse.tsx
@@ -1,5 +1,99 @@
+import { Combobox, Transition } from "@headlessui/react";
+import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/24/outline";
 import * as React from "react";
 
+import { ApiInfo, type ApiInfoValue } from "../../lib/apiInfo";
+
+const apis: string[] = [];
+for (const key in ApiInfo) {
+  apis.push(ApiInfo[key].name);
+}
+
 export default function Browse() {
-  return <div>Browse page</div>;
+  const [selectedApi, setSelectedApi] = React.useState(apis[0]);
+  const [query, setQuery] = React.useState("");
+
+  const filteredApis =
+    query === ""
+      ? apis
+      : apis.filter((api) => {
+          return api.toLowerCase().includes(query.toLowerCase());
+        });
+
+  const filterBar = (
+    <div className="fixed top-16 w-72">
+      <Combobox value={selectedApi} onChange={setSelectedApi}>
+        <div className="relative mt-1">
+          <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+            <Combobox.Input
+              className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+              displayValue={(api: string) => api ?? ""}
+              onChange={(event) => setQuery(event.target.value)}
+            />
+            <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+              <ChevronUpDownIcon
+                className="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+            </Combobox.Button>
+          </div>
+          <Transition
+            as={React.Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery("")}
+          >
+            <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              {filteredApis.length === 0 && query !== "" ? (
+                <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
+                  Nothing found.
+                </div>
+              ) : (
+                filteredApis.map((api) => (
+                  <Combobox.Option
+                    key={api}
+                    className={({ active }) =>
+                      `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                        active ? "bg-teal-600 text-white" : "text-gray-900"
+                      }`
+                    }
+                    value={api}
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        <span
+                          className={`block truncate ${
+                            selected ? "font-medium" : "font-normal"
+                          }`}
+                        >
+                          {api}
+                        </span>
+                        {selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                              active ? "text-white" : "text-teal-600"
+                            }`}
+                          >
+                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Combobox.Option>
+                ))
+              )}
+            </Combobox.Options>
+          </Transition>
+        </div>
+      </Combobox>
+    </div>
+  );
+
+  return (
+    <>
+      {filterBar}
+      <div>Hello from Browse</div>
+    </>
+  );
 }
