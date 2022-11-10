6a9aea29bd72f293a9dbb409d376a9d3e497deb9
sednaman <75589522+sednaman@users.noreply.github.com>
2022-11-10 17:27:07 -0600

    Added yarn library and started on editor integration, not currently working

--- src/routes/edit/Edit.tsx
+++ src/routes/edit/Edit.tsx
@@ -1,6 +1,32 @@
 import * as React from 'react';
 // import { useState, useEffect } from 'react';
 
+import ImageEditor from "@toast-ui/react-image-editor";
+import "tui-image-editor/dist/tui-image-editor.css";
+
 export default function Edit() {
-  return <div>Hello World!</div>
+  return (
+    <ImageEditor
+      includeUI={{
+        loadImage: {
+          path: "img/sampleImage.jpg",
+          name: "SampleImage",
+        },
+        menu: ["shape", "filter"],
+        initMenu: "filter",
+        uiSize: {
+          width: "1000px",
+          height: "700px",
+        },
+        menuBarPosition: "bottom",
+      }}
+      cssMaxHeight={500}
+      cssMaxWidth={700}
+      selectionStyle={{
+        cornerSize: 20,
+        rotatingPointOffset: 70,
+      }}
+      usageStatistics={true}
+    />
+  );
 }
