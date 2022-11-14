dcdd5758e75f54e4cb36b9603a3c8004e8852743
bob737373 <rfahey2@lsu.edu>
2022-11-13 20:04:52 -0600

    Integrated Firebase with Browsing

--- src/index.tsx
+++ src/index.tsx
@@ -4,52 +4,18 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import {
-  getFunctions,
-  connectFunctionsEmulator,
-  httpsCallable,
-} from "firebase/functions";
-
 import type { ImageAsset } from "../stare-into-the-void-functions/src/models/image-assets";
-import { FirebaseService } from "./lib/firebase-service";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initFirebase, FunctionsService } from './lib/firebase-services'
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBJZer3Ud9W6LehR-GgJhm52zyxN9XLNGo",
-  authDomain: "stare-into-the-void.firebaseapp.com",
-  projectId: "stare-into-the-void",
-  storageBucket: "stare-into-the-void.appspot.com",
-  messagingSenderId: "144334433499",
-  appId: "1:144334433499:web:ec88283f1e623cd02c70fe",
-  measurementId: "G-2868R29G5L",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const functions = getFunctions(app);
-if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-  console.log("connecting to emulator");
-  connectFunctionsEmulator(functions, "localhost", 5001);
-}
-/* const analytics =*/ getAnalytics(app);
-
-const apod = httpsCallable<undefined, ImageAsset>(functions, "apod", {});
-const nivl = httpsCallable<any, ImageAsset>(functions, "nivl", {});
-
-const firebaseService = new FirebaseService(apod, nivl);
+initFirebase();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App bgUrl={firebaseService.getPictureOfTheDay()} />
+    <App bgUrl={FunctionsService.instance.getPictureOfTheDay()}  />
   </React.StrictMode>
 );
 
