4d1c5870168707f536d79b2032f419848e31722b
sednaman <75589522+sednaman@users.noreply.github.com>
2022-11-14 14:24:19 -0600

    Jimmy: colors, buttons, and icons look cleaner in this

--- src/routes/edit/Edit.tsx
+++ src/routes/edit/Edit.tsx
@@ -6,80 +6,84 @@ import "tui-image-editor/dist/tui-image-editor.css";
 
 const theme = {
   "common.bi.image":
-    "https://i.pinimg.com/originals/5e/38/39/5e3839b3a6c248f9b7d794bf1ef67cf5.jpg",
+  "https://www.colorhexa.com/374151.png",
   "common.bisize.width": "50px",
   "common.bisize.height": "50px",
   "common.backgroundImage": "none",
-  "common.backgroundColor": "#1e1e1e",
+  "common.backgroundColor": "#374151",
   "common.border": "0px",
 
-  // header
-  "header.backgroundImage": "none",
-  "header.backgroundColor": "transparent",
-  "header.border": "0px",
+    // header
+    "header.backgroundImage": "none",
+    "header.backgroundColor": "transparent",
+    "header.border": "0px",
+  
+    // load button
+    "loadButton.backgroundColor": "#fff",
+    "loadButton.border": "1px solid #ddd",
+    "loadButton.color": "#222",
+    "loadButton.fontFamily": "NotoSans, sans-serif",
+    "loadButton.fontSize": "12px",
+  
+    // download button
+    "downloadButton.backgroundColor": "#1f2937",
+    "downloadButton.border": "1px solid #fff",
+    "downloadButton.color": "#fff",
+    "downloadButton.fontFamily": "NotoSans, sans-serif",
+    "downloadButton.fontSize": "12px",
+  
+    "menu.backgroundColor": "#1f2937",
 
-  // load button
-  "loadButton.backgroundColor": "#fff",
-  "loadButton.border": "1px solid #ddd",
-  "loadButton.color": "#222",
-  "loadButton.fontFamily": "NotoSans, sans-serif",
-  "loadButton.fontSize": "12px",
+    // icons default
+    "menu.normalIcon.color": "#8a8a8a",
+    "menu.activeIcon.color": "#fff",
+    "menu.disabledIcon.color": "#555555",
+    "menu.hoverIcon.color": "#e9e9e9",
+    "submenu.normalIcon.color": "#8a8a8a",
+    "submenu.activeIcon.color": "#e9e9e9",
+  
+    "menu.iconSize.width": "24px",
+    "menu.iconSize.height": "24px",
+    "submenu.iconSize.width": "32px",
+    "submenu.iconSize.height": "32px",
+  
+    // submenu primary color
+    "submenu.backgroundColor": "#1f2937",
+    "submenu.partition.color": "#858585",
+  
+    // submenu labels
+    "submenu.normalLabel.color": "#1f2937",
+    "submenu.normalLabel.fontWeight": "lighter",
+    "submenu.activeLabel.color": "#fff",
+    "submenu.activeLabel.fontWeight": "lighter",
 
-  // download button
-  "downloadButton.backgroundColor": "#fdba3b",
-  "downloadButton.border": "1px solid #fdba3b",
-  "downloadButton.color": "#fff",
-  "downloadButton.fontFamily": "NotoSans, sans-serif",
-  "downloadButton.fontSize": "12px",
-
-  // icons default
-  "menu.normalIcon.color": "#8a8a8a",
-  "menu.activeIcon.color": "#555555",
-  "menu.disabledIcon.color": "#434343",
-  "menu.hoverIcon.color": "#e9e9e9",
-  "submenu.normalIcon.color": "#8a8a8a",
-  "submenu.activeIcon.color": "#e9e9e9",
-
-  "menu.iconSize.width": "24px",
-  "menu.iconSize.height": "24px",
-  "submenu.iconSize.width": "32px",
-  "submenu.iconSize.height": "32px",
-
-  // submenu primary color
-  "submenu.backgroundColor": "#1e1e1e",
-  "submenu.partition.color": "#858585",
-
-  // submenu labels
-  "submenu.normalLabel.color": "#858585",
-  "submenu.normalLabel.fontWeight": "lighter",
-  "submenu.activeLabel.color": "#fff",
-  "submenu.activeLabel.fontWeight": "lighter",
-
-  // checkbox style
-  "checkbox.border": "1px solid #ccc",
-  "checkbox.backgroundColor": "#fff",
-
-  // rango style
-  "range.pointer.color": "#fff",
-  "range.bar.color": "#666",
-  "range.subbar.color": "#d1d1d1",
-
-  "range.disabledPointer.color": "#414141",
-  "range.disabledBar.color": "#282828",
-  "range.disabledSubbar.color": "#414141",
-
-  "range.value.color": "#fff",
-  "range.value.fontWeight": "lighter",
-  "range.value.fontSize": "11px",
-  "range.value.border": "1px solid #353535",
-  "range.value.backgroundColor": "#151515",
-  "range.title.color": "#fff",
-  "range.title.fontWeight": "lighter",
-
-  // colorpicker style
-  "colorpicker.button.border": "1px solid #1e1e1e",
-  "colorpicker.title.color": "#fff",
-};
+    
+  
+    // checkbox style
+    "checkbox.border": "1px solid #ccc",
+    "checkbox.backgroundColor": "#fff",
+  
+    // rango style
+    "range.pointer.color": "#fff",
+    "range.bar.color": "#666",
+    "range.subbar.color": "#d1d1d1",
+  
+    "range.disabledPointer.color": "#414141",
+    "range.disabledBar.color": "#282828",
+    "range.disabledSubbar.color": "#414141",
+  
+    "range.value.color": "#fff",
+    "range.value.fontWeight": "lighter",
+    "range.value.fontSize": "11px",
+    "range.value.border": "1px solid #353535",
+    "range.value.backgroundColor": "#151515",
+    "range.title.color": "#fff",
+    "range.title.fontWeight": "lighter",
+  
+    // colorpicker style
+    "colorpicker.button.border": "1px solid #1e1e1e",
+    "colorpicker.title.color": "#fff",
+  };
 
 interface EditProps {
   imgUrl?: string;
